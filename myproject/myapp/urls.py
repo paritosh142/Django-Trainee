4b06310a5b11774abaf4ccedf5b4c6f28a9a1480
Paritosh <79096636+paritosh142@users.noreply.github.com>
2024-09-18 16:14:25 +0530

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-user/', views.create_user),
+    path('create-user/', views.create_user),  # Question 1: Synchronous
+    path('create-user-thread/', views.create_user_thread),  # Question 2: Same Thread
+    path('create-user-transaction/', views.create_user_with_transaction),  # Question 3: Same Transaction
 ]
