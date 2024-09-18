cc4e23cf7d4a6db1e44cf40f73d42d0a501d0476
Paritosh <79096636+paritosh142@users.noreply.github.com>
2024-09-18 11:15:05 +0530

    Init

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
+
+
+def create_user(request):
+    user = User.objects.create(username="testuser")
+    return HttpResponse(f"User {user.username} created, signal triggered.")
