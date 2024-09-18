3911136d3c0c3108624d7686da84e68cf868bed6
Paritosh <79096636+paritosh142@users.noreply.github.com>
2024-09-18 16:13:03 +0530

    Update signals.py

--- myproject/myapp/signals.py
+++ myproject/myapp/signals.py
@@ -1,10 +1,21 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User
+import threading
 import time
+
+## Signal Handler for Question 1: Demonstrating synchronous execution
 
 @receiver(post_save, sender=User)
 def user_saved_handler(sender, instance, **kwargs):
-    print(f"Signal started for user: {instance.username}")
-    time.sleep(3)  # Simulate a time-consuming process
-    print(f"Signal finished for user: {instance.username}")
+    print("Signal handler started.")
+    time.sleep(5)  # Simulate long-running process
+    print("Signal handler finished.")
+
+
+
+## Signal Handler for Question 2: Demonstrating same thread execution
+@receiver(post_save, sender=User)
+def user_saved_thread_handler(sender, instance, **kwargs):
+    current_thread = threading.current_thread()
+    print(f"Signal handler running in thread: {current_thread.name}")
